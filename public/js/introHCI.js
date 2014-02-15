9421013e28da1e75ea71e63c9e24bcb596f9cda4
ZayneS <zheng.shen05@gmail.com>
2014-02-14 21:08:16 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,11 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	console.log("User clicked on project: " + idNumber);
+	var URL = "/project/" + idNumber;
+	console.log(URL);
+
+	$.get(URL, addProject);
 }
 
 /*
@@ -35,4 +39,26 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette', getRandomColor);
+}
+
+function getRandomColor(result) {
+	var colors = result['colors']['hex'];
+	console.log(colors);
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+
+function addProject(result) {
+	console.log(result);
+	var selector = "#project" + result['id'] + " .details";
+	var projectHTML = '<img src="' + result['image'] + '" class="img detailsImg">' + '<p>' + result['date'] + '</p>' + '<p>' + result['summary'] + '</p>';
+
+	$(selector).html(projectHTML);
+
+	// console.log(projectHTML);
 }
